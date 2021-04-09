5d5f66674425541510204cdbb3fc054804723291
Aliaksei Baranouski <aliaksei.baranouski@digitech7.by>
2021-04-09 21:23:11 +0300

    Added output of entered data

--- ui.frontend/src/components/FormWithModal/FormWithModal.js
+++ ui.frontend/src/components/FormWithModal/FormWithModal.js
@@ -1,15 +1,22 @@
 import React, {Component} from 'react';
-import {Button, Form, Modal} from 'react-bootstrap';
+import {Button, Form, Modal, Table} from 'react-bootstrap';
 
 export default class FormWithModal extends Component {
     state = {
-        isOpen: false
+        isOpen: false,
+        customer: {
+            firstname: "",
+            lastname: "",
+            email: "",
+            phone: "",
+            productinterest: ""
+        }
     };
 
     showModal = () => {
         this.setState({isOpen: true});
         const self = this;
-        setTimeout(function() {
+        setTimeout(function () {
             self.hideModal();
         }, 10000);
     };
@@ -17,6 +24,13 @@ export default class FormWithModal extends Component {
     hideModal = () => {
         this.setState({isOpen: false});
     };
+
+    handleFormChange(e) {
+        let customer = this.state.customer;
+        customer[e.target.id] = e.target.value;
+        this.setState({customer: customer});
+    };
+
 
     handleSubmit = (e) => {
         e.preventDefault();
@@ -52,23 +66,27 @@ export default class FormWithModal extends Component {
                 <Form onSubmit={this.handleSubmit}>
                     <Form.Group controlId="firstname">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="First Name" required/>
+                        <Form.Control type="text" placeholder="First Name" onChange={this.handleFormChange.bind(this)}
+                                      required/>
                     </Form.Group>
                     <Form.Group controlId="lastname">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Last Name" required/>
+                        <Form.Control type="text" placeholder="Last Name" onChange={this.handleFormChange.bind(this)}
+                                      required/>
                     </Form.Group>
                     <Form.Group controlId="email">
                         <Form.Label>Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="Email Address" required/>
+                        <Form.Control type="email" placeholder="Email Address"
+                                      onChange={this.handleFormChange.bind(this)} required/>
                     </Form.Group>
                     <Form.Group controlId="phone">
                         <Form.Label>Phone Number (US phone format)</Form.Label>
-                        <Form.Control type="tel" placeholder="Phone Number (US phone format)" required/>
+                        <Form.Control type="tel" placeholder="Phone Number (US phone format)"
+                                      onChange={this.handleFormChange.bind(this)} required/>
                     </Form.Group>
                     <Form.Group controlId="productinterest">
                         <Form.Label>How did you hear about us?</Form.Label>
-                        <Form.Control as="select" required>
+                        <Form.Control as="select" onChange={this.handleFormChange.bind(this)} required>
                             <option></option>
                             <option>Google</option>
                             <option>Fleetcor</option>
@@ -89,9 +107,34 @@ export default class FormWithModal extends Component {
                     show={this.state.isOpen}
                     onHide={() => void 0}>
                     <Modal.Header>
-                        <Modal.Title>Large Modal</Modal.Title>
+                        <Modal.Title>Successfully sent!</Modal.Title>
                     </Modal.Header>
-                    <Modal.Body>Collected Data</Modal.Body>
+                    <Modal.Body>
+                        <Table bordered>
+                            <tbody>
+                            <tr>
+                                <td>First Name</td>
+                                <td>{this.state.customer.firstname}</td>
+                            </tr>
+                            <tr>
+                                <td>Last Name</td>
+                                <td>{this.state.customer.lastname}</td>
+                            </tr>
+                            <tr>
+                                <td>Email Address</td>
+                                <td>{this.state.customer.email}</td>
+                            </tr>
+                            <tr>
+                                <td>Phone Number</td>
+                                <td>{this.state.customer.phone}</td>
+                            </tr>
+                            <tr>
+                                <td>How did you hear about us</td>
+                                <td>{this.state.customer.productinterest}</td>
+                            </tr>
+                            </tbody>
+                        </Table>
+                    </Modal.Body>
                 </Modal>
             </div>
         );
