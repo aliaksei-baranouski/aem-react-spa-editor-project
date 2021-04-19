a3f70f91c67cc70d33325759f8833d47b4babb0d
Aliaksei Baranouski <aliaksei.baranouski@digitech7.by>
2021-04-19 11:24:46 +0300

    Added error handling

--- ui.frontend/src/components/FormWithModal/FormWithModal.js
+++ ui.frontend/src/components/FormWithModal/FormWithModal.js
@@ -14,6 +14,7 @@ class FormWithModal extends Component {
     state = {
         isOpen: false,
         seconds: 10,
+        error: null,
         customer: {
             firstname: "",
             lastname: "",
@@ -29,10 +30,12 @@ class FormWithModal extends Component {
         this.myInterval = setInterval(() => {
             this.setState(({seconds}) => ({
                 seconds: seconds - 1
-            }))}, 1000)
+            }))
+        }, 1000)
         setTimeout(function () {
             self.hideModal();
         }, 10000);
+        self.setState({seconds: 10});
     };
 
     hideModal = () => {
@@ -47,20 +50,23 @@ class FormWithModal extends Component {
         this.setState({customer: customer});
     };
 
-    handleSubmit = (e) => {
+    handleSubmit = async (e) => {
         e.preventDefault();
 
         //TODO: Adobe Granite CSRF Filter
         try {
-            const response = fetch("/bin/salesforce", {
+            const response = await fetch("/bin/salesforce", {
                 method: 'POST',
                 body: JSON.stringify(this.state.customer),
                 headers: {
                     'Content-Type': 'application/json'
                 }
             });
-            console.log('Data sent successfully:', JSON.stringify(response));
+            if (!response.ok) {
+                throw Error(response.statusText);
+            }
         } catch (error) {
+            this.setState({error: error});
             console.error('Error:', error);
         }
 
@@ -82,22 +88,22 @@ class FormWithModal extends Component {
                 <Form onSubmit={this.handleSubmit}>
                     <Form.Group controlId="firstname">
                         <Form.Label>First Name</Form.Label>
-                        <Form.Control type="text" placeholder="First Name" onChange={this.handleFormChange.bind(this)}
-                                      required/>
+                        <Form.Control type="text" placeholder="First Name" pattern="^[A-Za-z]+$"
+                                      onChange={this.handleFormChange.bind(this)} required/>
                     </Form.Group>
                     <Form.Group controlId="lastname">
                         <Form.Label>Last Name</Form.Label>
-                        <Form.Control type="text" placeholder="Last Name" onChange={this.handleFormChange.bind(this)}
-                                      required/>
+                        <Form.Control type="text" placeholder="Last Name" pattern="^[A-Za-z]+$"
+                                      onChange={this.handleFormChange.bind(this)} required/>
                     </Form.Group>
                     <Form.Group controlId="email">
                         <Form.Label>Email Address</Form.Label>
-                        <Form.Control type="email" placeholder="Email Address"
+                        <Form.Control type="email" placeholder="Email Address" pattern="[^@]+@[^\.]+\..+"
                                       onChange={this.handleFormChange.bind(this)} required/>
                     </Form.Group>
                     <Form.Group controlId="phone">
                         <Form.Label>Phone Number (US phone format)</Form.Label>
-                        <Form.Control type="tel" placeholder="Phone Number (US phone format)"
+                        <Form.Control type="tel" placeholder="Phone Number (US phone format)" pattern="^[0-9]{7,11}$"
                                       onChange={this.handleFormChange.bind(this)} required/>
                     </Form.Group>
                     <Form.Group controlId="productinterest">
@@ -119,7 +125,31 @@ class FormWithModal extends Component {
                     show={this.state.isOpen}
                     onHide={() => void 0}>
                     <Modal.Header>
-                        <Modal.Title>Successfully sent!</Modal.Title>
+                        <div>
+                            {this.state.error ? (
+                                <Modal.Title>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         className="bi bi-exclamation-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                        <path
+                                            d="M7.002 11a1 1 0 1 1 2 0 1 1 0 0 1-2 0zM7.1 4.995a.905.905 0 1 1 1.8 0l-.35 3.507a.552.552 0 0 1-1.1 0L7.1 4.995z"/>
+                                    </svg>
+                                    &nbsp;Something went wrong!
+                                </Modal.Title>
+                            ) : (
+                                <Modal.Title>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                         className="bi bi-check-square" viewBox="0 0 16 16">
+                                        <path
+                                            d="M14 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H2a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h12zM2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2z"/>
+                                        <path
+                                            d="M10.97 4.97a.75.75 0 0 1 1.071 1.05l-3.992 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.235.235 0 0 1 .02-.022z"/>
+                                    </svg>
+                                    &nbsp;Successfully sent!
+                                </Modal.Title>
+                            )}
+                        </div>
                     </Modal.Header>
                     <Modal.Body>
                         <Table bordered>
